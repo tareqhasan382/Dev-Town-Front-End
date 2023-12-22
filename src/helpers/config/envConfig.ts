6775f3163fb32846f7d1f48e8e9a6756a745fbec
tareqhasan382 <tareqhasan382@gmail.com>
2023-12-22 16:39:29 +0600

    fix responsive

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = (): string => {
-  return "http://localhost:5000";
+  return "https://dev-town-backend.vercel.app";
 };
 
 // http://localhost:5000/api/v1/products || https://alemeno-backend-eight.vercel.app
